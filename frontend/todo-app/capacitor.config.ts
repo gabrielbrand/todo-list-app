c83cc0017409663958b6c7cf03ad22635ba9960b
gabrielbrand <42659911+gabrielbrand@users.noreply.github.com>
2025-04-07 08:16:13 -0400

    criação Dockerfile

---
+++ frontend/todo-app/capacitor.config.ts
@@ -0,0 +1,8 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'example',
+  webDir: 'out', // Ensure this matches your Next.js output directory
+};
+export default config;
